4c62de8910d11cf6ee8cec2ce3fe150ef47f2bc1
CHO <cho@champloo.co.jp>
2012-07-30 11:35:11 +0900

    Inital commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Cho::Application.config.secret_token = 'c36e01ff2369d5b4b884aab618d6a3c657ceecf3eb81fb7916347909a259582016010dff80c156fbc9157c4dc81e98a77c4817b080de437df0978cfc4f6327cb'
